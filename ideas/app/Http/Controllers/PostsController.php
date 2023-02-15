e093c2eba48ac91e919c1e359b2738bada2d3a9e
najoua belhaj <najouabelhaj7@gmail.com>
2023-02-15 17:34:26 +0100

    commit

---
+++ ideas/app/Http/Controllers/PostsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    //   public function index(Request $request ){
+    //     $nom=$request->nom;
+    //     return view('home',[
+    //         'nom' =>$nom
+    //     ]);
+    // }
+}
